e993aa06eaeacc8b5f60ffba4e360d75de0d2c98
Hector Garcia Millares <hgarcia.millares@gmail.com>
2020-11-03 22:37:38 -0500

    Annotated .js code and added code for current time display using moment.js to be deployed later

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Code to determine how time blocks get css colour displayed
 var timeNow= new Date().getHours();
 
 if (timeNow > 8) {
@@ -99,7 +100,7 @@ if (timeNow > 17) {
     else if (timeNow < 17) {
         $("#note9").addClass("future");
     }
-if (timeNow > 18 {
+if (timeNow > 18) {
     $("#note10").addClass("past");
     }
     else if (timeNow >= 18 && timeNow < 19){
@@ -108,9 +109,24 @@ if (timeNow > 18 {
     else if (timeNow < 18) {
         $("#note10").addClass("future");
     }
-    
+
+
+    // 8AM Task code to commit to local storage and retrieve value
+    var input_textarea0 = document.querySelector('note0');
+    var save_button0 = document.querySelector('#save8am');
+
+    input_textarea0.value = localStorage.getItem('content0');
+    debugger;
+
+    saveNote1.addEventListener('click', updateOutput0);
+
+    function updateOutput0() {
+    localStorage.setItem('content0', input_textarea1.value);
+    };
+
+    //9AM Task code to commit to local storage and retrieve value
     var input_textarea1 = document.querySelector('#note1');
-    var save_button1 = document.querySelector('#saveNote1');
+    var save_button1 = document.querySelector('#save9am');
 
     input_textarea1.value = localStorage.getItem('content1');
 
@@ -120,90 +136,121 @@ if (timeNow > 18 {
     localStorage.setItem('content1', input_textarea1.value);
     };
 
+    //10AM Task code to commit to local storage and retrieve value
     var input_textarea2 = document.querySelector('#note2');
-    var save_button2 = document.querySelector('#saveNote2');
+    var save_button2 = document.querySelector('#save10am');
 
     input_textarea2.value = localStorage.getItem('content2');
-//when the Save button is clicked, store the input of the textarea
+
     saveNote2.addEventListener('click', updateOutput2);
-//sets the input of text area into local storage
+
     function updateOutput2() {
 	localStorage.setItem('content2', input_textarea2.value);
     };
 
+    //11AM Task code to commit to local storage and retrieve value
     var input_textarea3 = document.querySelector('#note3');
-    var save_button3 = document.querySelector('#saveNote3');
-//retrieves previously save text to display
+    var save_button3 = document.querySelector('#save11am');
+
     input_textarea3.value = localStorage.getItem('content3');
-//when the Save button is clicked, store the input of the textarea
+
     saveNote3.addEventListener('click', updateOutput3);
-//sets the input of text area into local storage
+
     function updateOutput3() {
 	localStorage.setItem('content3', input_textarea3.value);
     };
 
+    //12PM Task code to commit to local storage and retrieve value
     var input_textarea4 = document.querySelector('#note4');
-    var save_button4 = document.querySelector('#saveNote4');
-//retrieves previously save text to display
+    var save_button4 = document.querySelector('#save12pm');
+
     input_textarea4.value = localStorage.getItem('content4');
-//when the Save button is clicked, store the input of the textarea
+
     saveNote4.addEventListener('click', updateOutput4);
-//sets the input of text area into local storage
+
     function updateOutput4() {
 	localStorage.setItem('content4', input_textarea4.value);
     };
 
+    //1PM Task code to commit to local storage and retrieve value
     var input_textarea5 = document.querySelector('#note5');
-    var save_button5 = document.querySelector('#saveNote5');
-//retrieves previously save text to display
+    var save_button5 = document.querySelector('#save1pm');
+
     input_textarea5.value = localStorage.getItem('content5');
-//when the Save button is clicked, store the input of the textarea
+
     saveNote5.addEventListener('click', updateOutput5);
-//sets the input of text area into local storage
+
     function updateOutput5() {
 	localStorage.setItem('content5', input_textarea5.value);
     };  
 
+    //2PM Task code to commit to local storage and retrieve value
     var input_textarea6 = document.querySelector('#note6');
-    var save_button6 = document.querySelector('#saveNote6');
-//retrieves previously save text to display
+    var save_button6 = document.querySelector('#save2pm');
+
     input_textarea6.value = localStorage.getItem('content6');
-//when the Save button is clicked, store the input of the textarea
+
     saveNote6.addEventListener('click', updateOutput6);
-//sets the input of text area into local storage
+
     function updateOutput6() {
 	localStorage.setItem('content6', input_textarea6.value);
     };
 
+    //3PM Task code to commit to local storage and retrieve value
     var input_textarea7 = document.querySelector('#note7');
-    var save_button7 = document.querySelector('#saveNote7');
-//retrieves previously save text to display
+    var save_button7 = document.querySelector('#save3pm');
+
     input_textarea7.value = localStorage.getItem('content7');
-//when the Save button is clicked, store the input of the textarea
+
     saveNote7.addEventListener('click', updateOutput7);
-//sets the input of text area into local storage
+
     function updateOutput7() {
 	localStorage.setItem('content7', input_textarea7.value);
     };
 
+    //4PM Task code to commit to local storage and retrieve value
     var input_textarea8 = document.querySelector('#note8');
-    var save_button8 = document.querySelector('#saveNote8');
-//retrieves previously save text to display
+    var save_button8 = document.querySelector('#save4pm');
+
     input_textarea8.value = localStorage.getItem('content8');
-//when the Save button is clicked, store the input of the textarea
+
     saveNote8.addEventListener('click', updateOutput8);
-//sets the input of text area into local storage
+
     function updateOutput8() {
 	localStorage.setItem('content8', input_textarea8.value);
     };
 
+    //5PM Task code to commit to local storage and retrieve value
     var input_textarea9 = document.querySelector('#note9');
-    var save_button9 = document.querySelector('#saveNote9');
-//retrieves previously save text to display
+    var save_button9 = document.querySelector('#save5pm');
+
     input_textarea9.value = localStorage.getItem('content9');
-//when the Save button is clicked, store the input of the textarea
+
     saveNote9.addEventListener('click', updateOutput9);
-//sets the input of text area into local storage
+
     function updateOutput9() {
 	localStorage.setItem('content9', input_textarea9.value);
     };
+
+    //6M Task code to commit to local storage and retrieve value
+    var input_textarea10 = document.querySelector('#note10');
+    var save_button10 = document.querySelector('#save6pm');
+
+    input_textarea10.value = localStorage.getItem('content10');
+
+    saveNote10.addEventListener('click', updateOutput10);
+
+    function updateOutput10() {
+	localStorage.setItem('content10', input_textarea10.value);
+    };
+
+
+
+    var sample = function(){
+        var a = b = 3;
+    }
+
+    sample();
+
+    console.log("Is a defined?", typeof a !== "undefined");
+    console.log("Is b defined?", typeof b !== "undefined");
